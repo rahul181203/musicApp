6124ba49a2ad06487152bb3b9df7840f1612bd43
rahul181203 <rahulkanakam123@gmail.com>
2024-01-14 14:16:42 +0530

    analytics

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { Theme } from "@radix-ui/themes";
 import { Provider } from "jotai";
 import "@radix-ui/themes/styles.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
@@ -30,6 +31,7 @@ export default function RootLayout({
       <Provider>
           <Theme appearance={"dark"} panelBackground={'solid'}>
             {children}
+            <Analytics/>
           </Theme>
       </Provider>
       </body>
