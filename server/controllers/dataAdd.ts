f6fb2e26b99c51dfede67f77f203ccb40012de30
rahul181203 <rahulkanakam123@gmail.com>
2024-01-21 00:52:22 +0530

    updates

--- server/controllers/dataAdd.ts
+++ server/controllers/dataAdd.ts
@@ -1,44 +1,44 @@
 import { PrismaClient } from '@prisma/client'
 import { Request,Response } from 'express'
 const prisma = new PrismaClient()
-import AlbumData from "../album.json";
-import Artistdata from "../artists.json";
-import songData from "../songs.json";
-import relation from "../songArtists.json";
+// import AlbumData from "../album.json";
+// import Artistdata from "../artists.json";
+// import songData from "../songs.json";
+// import relation from "../songArtists.json";
 
-export const AddArtist=async(req:Request,res:Response)=>{
-    await prisma.artist.createMany({
-        data:Artistdata
-    }).then(()=>{
-        res.status(200).json({result:"added sucessfully"});
-    }).catch((err)=>{
-        console.log(err);
-    })
-}
+// export const AddArtist=async(req:Request,res:Response)=>{
+//     await prisma.artist.createMany({
+//         data:Artistdata
+//     }).then(()=>{
+//         res.status(200).json({result:"added sucessfully"});
+//     }).catch((err)=>{
+//         console.log(err);
+//     })
+// }
 
-export const AddSong=async(req:Request,res:Response)=>{
-    await prisma.song.createMany({
-        data:songData
-    }).then(()=>{
-        res.status(200).json({result:"Added successfully"});
-    })
-}
+// export const AddSong=async(req:Request,res:Response)=>{
+//     await prisma.song.createMany({
+//         data:songData
+//     }).then(()=>{
+//         res.status(200).json({result:"Added successfully"});
+//     })
+// }
 
-export const AddSongArtist=async(req:Request,res:Response)=>{
-    await prisma.songArtist.createMany({
-        data:relation
-    }).then(()=>{
-        res.status(200).json({result:"Added successfully"});
-    })
-}
+// export const AddSongArtist=async(req:Request,res:Response)=>{
+//     await prisma.songArtist.createMany({
+//         data:relation
+//     }).then(()=>{
+//         res.status(200).json({result:"Added successfully"});
+//     })
+// }
 
-export const AddAlbums=async(req:Request,res:Response)=>{
-    await prisma.album.createMany({
-        data: AlbumData
-    }).then(()=>{
-        res.status(200).json({result:"added sucessfully"});
-    })
-}
+// export const AddAlbums=async(req:Request,res:Response)=>{
+//     await prisma.album.createMany({
+//         data: AlbumData
+//     }).then(()=>{
+//         res.status(200).json({result:"added sucessfully"});
+//     })
+// }
 
 export const getAlbums=async(req:Request,res:Response)=>{
     const data = await prisma.album.findMany({
